b7d4bba918b4fe3c3e814a883b267803cd317a52
Billy Editiano <billy.editiano@datasaur.ai>
2023-09-04 09:26:26 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "thebucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_tag_name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  description = "Name of the bucket"
+}
+
+variable "bucket_tag_name" {
+  description = "bucket tag name"
+  default = "testing"
+}
